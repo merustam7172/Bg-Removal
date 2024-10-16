d196177134a6828a634c04d9271bf56d11525208
merustam7172 <merustam7172@gmail.com>
2024-10-16 21:50:45 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js';
-import axios from 'axios';
+
 // app config
 const PORT = process.env.PORT || 8000;
 const app = express();
