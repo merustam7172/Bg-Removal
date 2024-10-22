192a3df42d16e43ffa0665b615ac7b59490322f4
merustam7172 <merustam7172@gmail.com>
2024-10-22 14:46:13 +0530

    fixing errors

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Header from '../components/Header'
-import Steps from '../components/steps'
 import BgSlider from '../components/BgSlider'
-import Testimonials from '../components/testimonials'
+import Testimonials from '../components/Testimonials'
 import Upload from '../components/Upload'
+import Steps from '../components/Steps'
 
 const Home = () => {
   return (
